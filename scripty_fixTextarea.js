2af82299e1396e5e8efc5306b49ffb1e4831e92c
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-08-30 17:55:50 +0200

    Actually, the weebsite building tool loads the textareas async

--- scripty_fixTextarea.js
+++ scripty_fixTextarea.js
@@ -11,6 +11,21 @@
 // USER SETTINGS
 
 // DANGER ZONE BELOW, CHANGE AT YOUR OWN RISK
-document.querySelectorAll('textarea').forEach(element => {
-	element.style.maxWidth = '100%';
+const scripty_fixTextarea_doFix = function() {
+	document.querySelectorAll('textarea').forEach(element => {
+		element.style.maxWidth = '100%';
+	})
+}
+
+const scripty_fixTextarea_observer = new MutationObserver(mutations => {
+	mutations.forEach(() => {
+		scripty_fixTextarea_doFix()
+	})
 })
+
+const scripty_fixTextarea_container = document.querySelector('body')
+if (scripty_fixTextarea_container) {
+	scripty_fixTextarea_observer.observe(scripty_fixTextarea_container, {childList: true, subtree: false})
+}
+
+scripty_fixTextarea_doFix()
